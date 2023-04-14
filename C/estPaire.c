b3b7c61ba9ea2c85c29b0bf7217d4e5e01e5da68
Seyni KANE <kaneseyni.sn@gmail.com>
2023-04-14 11:15:20 +0200

    This is basic algorithm which test whether an given integer is even or odd

--- C/estPaire.c
+++ C/estPaire.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+//This is basic algorithm which test whether an given integer is even or odd
+
+int main(int argc, char const *argv[]) {
+  int nb = 0;
+  printf("Ce programme permet de tester si un nombre est paire !\n\n");
+  do {
+    printf("Donner un entier :\n");
+    scanf("%d", &nb);
+  } while(nb < 0 );
+  if (nb % 2 == 0){
+    printf("Le nombre %d est paire\n", nb);
+  }
+  else{
+    printf("Le nombre %d n'est pas paire\n", nb);
+  }
+  return 0;
+}
