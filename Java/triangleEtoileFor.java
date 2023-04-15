9eabedb94282add8696cc4dfdbcf1b9cfd1abb0b
Seyni KANE <kaneseyni.sn@gmail.com>
2023-04-15 22:17:55 +0200

    Display an star trangle

---
+++ Java/triangleEtoileFor.java
@@ -0,0 +1,19 @@
+//This class display a triangle of stars
+public class triangleEtoileFor
+  {
+    public static void main(String[] args)
+      {
+
+        //int n = Integer.parseInt(args[0]);
+        int n = 10;
+        for(int i = 0; i < n; i++)
+          {
+            for(int j = 0; j < i; j++)
+              {
+                System.out.print("*");
+              }
+            System.out.println("");
+          }
+
+      }
+  }
